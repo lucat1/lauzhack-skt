a5890ac3f33e79e8b288341fcce44e1eaab1cf1a
Andreea Scrob <andreeascrob@icloud.com>
2023-12-02 21:51:10 +0100

    create button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SbbButtonModule } from '@sbb-esta/angular/button';
 import { SbbCheckboxModule } from '@sbb-esta/angular/checkbox';
 import { JourneyMapsBasicExample } from './components/map-component/map-component.component';
 import { InputOverviewExample } from './components/input-component/input-component.component';
+import { ButtonComponentComponent } from './components/button-component/button-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,JourneyMapsBasicExample,InputOverviewExample],
+  imports: [CommonModule, RouterOutlet,JourneyMapsBasicExample,InputOverviewExample, ButtonComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

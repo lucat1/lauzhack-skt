c8bb38326b30a2d96828f259f9f1856eab558e4e
Andreea Scrob <andreeascrob@icloud.com>
2023-12-03 09:40:05 +0100

    fix ListComponent border

--- frontVERO/src/components/ListComponent.tsx
+++ frontVERO/src/components/ListComponent.tsx
@@ -28,8 +28,8 @@ const ListComponent = () => {
 
   return (
     <div className="flex justify-center">
-    <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-      <li className="pb-3 sm:pb-4 border-2 cursor-pointer" >
+    <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700 ">
+      <li className="pb-3 sm:pb-4 border-1 cursor-pointer rounded" >
         <div className="flex flex-row">
           <div className="flex items-center space-x-4 rtl:space-x-reverse p-5">
             {paths.slice(0, maxVisiblePaths).map((path, index) => {

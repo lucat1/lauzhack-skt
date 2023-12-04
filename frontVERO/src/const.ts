c62506531303b22da66fd7d74ae787911326f3d1
Luca <luca@teapot.ovh>
2023-12-04 09:34:00 +0100

    update local url

--- frontVERO/src/const.ts
+++ frontVERO/src/const.ts
@@ -1,2 +1,2 @@
-export const URL_SEARCH = "https://35c03f774250714ddd2422d5dd7c5959.serveo.net";
+export const URL_SEARCH = "http://localhost:5000" // "https://35c03f774250714ddd2422d5dd7c5959.serveo.net";
 export const API_KEY_MAPS = "4a6dca7396895a262d76584d7b203f8e";
